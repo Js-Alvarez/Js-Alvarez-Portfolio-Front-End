8f9487cb25c7f021ff5be041e5979c9e2f58a383
Alvares Js <alvarezjs1990@gmail.com>
2023-11-12 21:59:44 -0300

    Se agrego fondo al proyecto y se corriguieron algunos estilos ademas darle funcionalidad atraves de JS

---
+++ script.js
@@ -0,0 +1,13 @@
+let toggle = document.getElementById('toggle');
+let label_toggle = document.getElementById('label_toggle');
+toggle.addEventListener('change', (event) => {
+    let checked = event.target.checked;
+    document.body.classList.toggle('dark');
+    if (checked == true) {
+        label_toggle.innerHTML = '&#9728;'; // Claro
+        label_toggle.style.fontSize = '40px';  
+        label_toggle.style.cursor = 'pointer';
+    } else {
+        label_toggle.innerHTML = '&#127771;'; // Oscuro
+    }
+});
