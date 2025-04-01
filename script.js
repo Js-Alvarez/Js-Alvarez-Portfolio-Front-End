d320312ee5b115a73366e022e7bce76be6585206
Alvarez Jesica <alvarezjs1990@gmail.com>
2025-04-01 11:49:46 -0300

    Se optimiza la seccion de proyectos con JavaScript

--- script.js
+++ script.js
@@ -1,45 +1,42 @@
-let toggle = document.getElementById('toggle');
-let label_toggle = document.getElementById('label_toggle');
-toggle.addEventListener('change', (event) => {
-    let checked = event.target.checked;
-    document.body.classList.toggle('dark');
-    if (checked == true) {
-        label_toggle.innerHTML = '&#9728;'; // Claro
-        label_toggle.style.fontSize = '40px';  
-        label_toggle.style.cursor = 'pointer';
-    } else {
-        label_toggle.innerHTML = '&#127771;'; // Oscuro
-    }
+//Proyectos
+const arrayProyectos = [
+    {
+        id:1,
+        nombre:"Amigo Invisible",
+        lenguaje:["JavaScript"],
+        descripcion: "Ideal para hacer sorteos en la escuela u oficina " 
+    },
+    {
+        id:2,
+        nombre:"Memomi Game",
+        lenguaje:["Vite", "JavaScript"],
+        descripcion: "Un divertido jugo para desafiar tu memoria" 
+    },
+    {
+        id:3,
+        nombre:"igo Invisible",
+        lenguaje:[],
+        descripcion: "" 
+    },
+    {
+        id:4,
+        nombre:"igo Invisible",
+        lenguaje:[],
+        descripcion: "" 
+    },
+ 
+    
+];
+
+const divProyecto = document.querySelector("#proyecto");
+
+arrayProyectos.forEach(proyecto => {
+    const divCodigo=document.createElement("div");
+
+    divCodigo.className="codigoProyecto";
+
+    divCodigo.addEventListener("click",()=>seleccionar(divCodigo,))
+    divProyecto.append(divCodigo)
+    
 });
-
-document.addEventListener('DOMContentLoaded', () => {
-    const carrusel = document.querySelector('.carrusel-contenedor');
-    const proyectos = document.querySelectorAll('.proyecto');
-    const anterior = document.querySelector('.carrusel-anterior');
-    const siguiente = document.querySelector('.carrusel-siguiente');
-    let contador = 0;
-
-    function mostrarProyecto(index) {
-        carrusel.style.transform = `translateX(${-index * 100}%)`;
-    }
-
-    anterior.addEventListener('click', () => {
-        contador = (contador > 0) ? contador - 1 : proyectos.length - 1;
-        mostrarProyecto(contador);
-    });
-
-    siguiente.addEventListener('click', () => {
-        contador = (contador < proyectos.length - 1) ? contador + 1 : 0;
-        mostrarProyecto(contador);
-    });
-
-    // Auto-play (optional)
-    setInterval(() => {
-        contador = (contador < proyectos.length - 1) ? contador + 1 : 0;
-        mostrarProyecto(contador);
-    }, 5000);
-});
-
-
-
 
